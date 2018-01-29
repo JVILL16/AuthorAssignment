1dcf8967b4766b2379a642378d33f6603117011f
jvill <jvill@172.16.4.88>
2018-01-28 22:12:14 -0600

    just in caase

--- Assignment1/src/List/MenuController.java
+++ Assignment1/src/List/MenuController.java
@@ -23,6 +23,7 @@ public class MenuController implements Initializable {
     public MenuController(ObservableList<Author> authors) {
         this.authors = authors;
     }
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
